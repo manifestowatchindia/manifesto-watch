0c75882fef8b7c826238a9a0bab7a8bb545b975c
ashwinipateriya@gmail.com <43302676+ashwinipateriya@users.noreply.github.com>
2025-03-30 19:53:15 +0530

    updating the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { ExploreTopTopics } from './layouts/Homepage/ExploreTopTopics';
 
 
 const App: React.FC = () => {
   return (
    <div>
     <Navbar />
+    <ExploreTopTopics/>
     <Footer />
     </div>
   );
