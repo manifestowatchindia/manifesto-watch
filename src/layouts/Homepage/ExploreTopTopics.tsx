e34eda176978da8fb146e3213492e60501109959
ashwinipateriya@gmail.com <43302676+ashwinipateriya@users.noreply.github.com>
2025-04-01 00:04:05 +0530

    updating the homepage removed content

--- src/layouts/Homepage/ExploreTopTopics.tsx
+++ src/layouts/Homepage/ExploreTopTopics.tsx
@@ -6,9 +6,9 @@ export const ExploreTopTopics: React.FC = () => {
                 <img src="/static/images/democracy_v2.jpg" alt="Democracy" className="image-section" />
                 <div className="overlay d-flex justify-content-center align-items-center">
                     <div className="text-center text-white">
-                        <h1 className="display-4 fw-bold">Empowering Voices, Shaping the Future.</h1>
-                        <p className="fs-4">Explore manifestos that shape tomorrow.</p>
-                        <a href="#explore" className="btn btn-primary">Explore NOW</a>
+                        <h1 className="display-4 fw-bold">Tracking Manifestos, Measuring Progress.</h1>
+                        
+                        
                     </div>
                 </div>
             </div>
